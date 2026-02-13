e682b73c0aa68f40708e922c03c5d05e358557a7
elciclon <adrianfernandezfazio@gmail.com>
2026-02-13 18:36:10 -0300

    Eliminando totales y asignando provincias, continuación

--- tp_labo_1_NAN.py
+++ tp_labo_1_NAN.py
@@ -48,8 +48,27 @@ censo_2010_df = censo_2010_df.drop(['CEPAL/CELADE Redatam+SP 01/29/2026',
 censo_2022_df = censo_2022_df.drop(['CEPAL/CELADE Redatam+SP 01/29/2026', 
                                     'Unnamed: 5'], axis = 1)
 
-
+censo_2010_df = censo_2010_df.reset_index(drop=True)
+censo_2022_df = censo_2022_df.reset_index(drop=True)
 #%% Elimina todas las tuplas con totales
+def elimina_totales(censo):
+    i = 0
+    
+    while i < len(censo):
+        if 'Total' in str(censo.loc[i,'Unnamed: 1']):# Castea a str por Nan
+            i+=1
+            while 'AREA' not in str(censo.loc[i,'Unnamed: 1']):# Castea a str por Nan
+                censo = censo.drop([i], axis = 0)
+                i+=1
+        i+=1
+    return censo
+
+censo_2010_df = elimina_totales(censo_2010_df)
+censo_2022_df = elimina_totales(censo_2022_df)
+
+censo_2010_df = censo_2010_df[censo_2010_df['Unnamed: 1'] != 'Total']
+censo_2022_df = censo_2022_df[censo_2022_df['Unnamed: 1'] != 'Total']
+
 censo_2010_df = censo_2010_df[censo_2010_df['Unnamed: 2'] != ' Total']
 censo_2022_df = censo_2022_df[censo_2022_df['Unnamed: 2'] != ' Total']
 
