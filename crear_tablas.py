cab03eeb737d5fc2ac2e280706b2ba4a5f65df1d
Antu Rozas <antrazas@outlook.com>
2026-02-15 16:27:05 +0000

    agrego informe y recursos

---
+++ crear_tablas.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Grupo: NAN
+Integrantes: Rozas Chavez, Antuanette Carolina
+             Madril, Joaquin Leandro
+             Fernández Fazio, Adrián Patricio
+             
+Este archivo incluye todo el código para recolectar los datos desde los censos,
+limpiarlos, y responder a los requerimientos de la consigna.
+
+"""
+
+#%%
+
+#%%
