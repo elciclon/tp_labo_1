e0460ecd5a04f230011a8489e31b67f2a80895d1
Antu Rozas <antrazas@outlook.com>
2026-02-16 16:54:22 +0000

    creacion de tablas

--- crear_tablas.py
+++ crear_tablas.py
@@ -23,3 +23,22 @@ instituciones = pd.read_csv('instituciones_limpio.csv', index_col=0)
 
 #%% Creamos la estructura de tablas
 
+provincia = pd.DataFrame(columns= ['id_provincia', 'nombre'])
+
+provincia = provincia.set_index('id_provincia')
+
+defuncion = pd.DataFrame(columns= ['id_provincia', 'causa', 'rango', 'sexo', 'año', 'cantidad'])
+
+defuncion = defuncion.set_index(['id_provincia', 'causa', 'rango', 'sexo', 'año'])
+
+grupoPoblacional = pd.DataFrame(columns= ['id_provincia', 'rango', 'sexo', 'año', 'tiene_cobertura', 'cantidad'])
+
+grupoPoblacional = grupoPoblacional.set_index(['id_provincia', 'rango', 'sexo', 'año', 'tiene_cobertura'])
+
+departamento = pd.DataFrame(columns= ['id_departamento', 'nombre', 'id_provincia'])
+
+departamento = departamento.set_index('id_departamento')
+
+establecimiento = pd.DataFrame(columns= ['id_establecimiento', 'financiamiento', 'tiene_terapia', 'id_departamento'])
+
+establecimiento = establecimiento.set_index('id_establecimiento')
