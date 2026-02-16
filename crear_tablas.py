7af924130ceca64e22607be97e72cf57a4e622e8
elciclon <adrianfernandezfazio@gmail.com>
2026-02-16 13:39:23 -0300

    Carga de archivos saneados para crear tablas del MER

--- crear_tablas.py
+++ crear_tablas.py
@@ -6,11 +6,20 @@ Integrantes: Rozas Chavez, Antuanette Carolina
              Madril, Joaquin Leandro
              Fernández Fazio, Adrián Patricio
              
-Este archivo incluye todo el código para recolectar los datos desde los censos,
-limpiarlos, y responder a los requerimientos de la consigna.
+Este archivo incluye todo el código para crear las tablas a partir de los 
+documentos provistos ya saneados, y responder a los requerimientos de la 
+consigna.
 
 """
 
-#%%
+#%% Cargamos archivos saneados
+import pandas as pd
 
-#%%
+censo_2010 = pd.read_csv('censo_2010_limpio.csv')
+censo_2022 = pd.read_csv('censo_2022_limpio.csv')
+defunciones = pd.read_csv('defunciones_limpio.csv')
+instituciones = pd.read_csv('instituciones_limpio.csv')
+
+
+#%% Creamos la estructura de tablas
+
